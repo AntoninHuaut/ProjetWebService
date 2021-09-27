8e0818d1c467e1c7203eca430124fc2eb6f83b6f
Antonin Huaut <antonin.huaut+github@gmail.com>
2021-09-27 16:29:50 +0200

    Fixing proxy with invalid body

--- ProxyService/src/Server.ts
+++ ProxyService/src/Server.ts
@@ -1,4 +1,4 @@
-import { Application } from "https://deno.land/x/oak@v9.0.1/mod.ts";
+import { Application, Context } from "https://deno.land/x/oak@v9.0.1/mod.ts";
 import { ConfigModule } from "./Config.ts";
 import Config = ConfigModule.Config;
 
@@ -22,7 +22,7 @@ export default class Server {
         await app.listen({ port: this.config.port });
     }
 
-    async proxy(ctx: any) {
+    async proxy(ctx: Context<any>) {
         const url: URL = ctx.request.url;
 
         const webService: string = url.pathname.split('/').filter(el => el).shift() ?? '';
@@ -44,21 +44,55 @@ export default class Server {
 
         const bodyRaw = ctx.request.body();
         if (bodyRaw.type === 'json' && bodyRaw.value) {
-            const body = await bodyRaw.value;
-            options.body = JSON.stringify(body);
+            const bodyValue = await bodyRaw.value;
+            const validBody: boolean = Array.isArray(bodyValue) || Object.keys(bodyValue).length > 0;
+
+            if (validBody) {
+                options.body = JSON.stringify(bodyValue);
+            }
+        }
+
+        if (this.shouldHaveBody(ctx.request.method) && !options.body) {
+            this.sendErrorResponse(ctx, 400, urlPath, 'Invalid body');
+            return;
         }
 
         try {
-            const response = await fetch(`${host}${urlPath}`, options);
-            ctx.response.status = response.status;
-            ctx.response.headers = response.headers;
-            ctx.response.body = response.body;
+            const proxyFetch: Promise<Response> = fetch(`${host}${urlPath}`, options);
+            const response: Response = await this.timeoutPromise(7000, new Error("Timeout"), proxyFetch)
+            this.sendResponse(ctx, response.status, response.body, response.headers);
         } catch (ex) {
-            ctx.response.status = 400;
-            ctx.response.body = {
-                error: ex.toString()
-            }
+            this.sendErrorResponse(ctx, 400, urlPath, ex.toString());
         }
+    }
+
+    sendErrorResponse(ctx: Context<any>, status: number, path: string, errorMsg: string) {
+        this.sendResponse(ctx, status, {
+            timestamp: new Date().toISOString(),
+            status: status,
+            error: errorMsg,
+            path: path
+        });
+    }
+
+    sendResponse(ctx: Context<any>, status: number, body: any, headers: Headers | undefined = undefined) {
+        ctx.response.status = status;
+        ctx.response.body = body;
+
+        if (headers) {
+            ctx.response.headers = headers;
+        }
+    }
+
+    shouldHaveBody(httpMethod: string): boolean {
+        return !['GET', 'HEAD'].includes(httpMethod?.toUpperCase());
+    }
+
+    timeoutPromise(timeout: number, err: Error, promise: Promise<Response>): Promise<Response> {
+        return new Promise(function (resolve, reject) {
+            promise.then(resolve, reject);
+            setTimeout(reject.bind(null, err), timeout);
+        });
     }
 
     getWebService(webService: string): string {

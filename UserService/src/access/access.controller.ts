2ac189098c85fe38c34c01f08936d19035465127
Antonin Huaut <antonin.huaut+github@gmail.com>
2021-10-08 10:31:27 +0200

    User: logout

--- UserService/src/access/access.controller.ts
+++ UserService/src/access/access.controller.ts
@@ -1,4 +1,4 @@
-import {Body, Controller, Get, Param, Post} from '@nestjs/common';
+import {Body, Controller, Delete, Get, Param, Post, Query, Req} from '@nestjs/common';
 import {AccessService} from './access.service';
 import {Action} from "./entities/action.enum";
 import {LoginUserDto} from "./dto/login-user-dto";
@@ -15,6 +15,11 @@ export class AccessController {
         return {
             access: bolRes
         }
+    }
+
+    @Delete('/:logout/:token')
+    async deleteToken(@Param('token') token: string) {
+        await this.accessService.deleteToken(token);
     }
 
     @Post('/login')
